d410c9079f8bd44a69c6c6a88edcf726af92a6c2
Anmol Chawla <anmolchawla10@gmail.com>
2019-06-05 18:55:55 +0530

    Improve the implementation of shared pointer

--- Smart Pointers/my_shared_pointer.cpp
+++ Smart Pointers/my_shared_pointer.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
 
-namespace my_std{&
+namespace my_std{
     template<typename T>
     class shared_ptr{
         T* data;
         int* count;
         public:
-            explicit shared_ptr(T* data): data{data}, count{new int(1)} {}
+            explicit shared_ptr(T* data)
+            try:
+                data{data},
+                count{new int(1)}
+                {}
+            catch(...){
+                delete data;
+                throw;
+            }
+
             ~shared_ptr() {
                 --(*count);
-                if(*count == 0)
+                if(*count == 0){
+                    std::cout<<"\nDeleting the data : "<<*data;
                     delete data;
+                    delete count;
+                }
             }
-            shared_ptr(shared_ptr const& copy) : data{copy.data}, count{copy.data}{
+
+            shared_ptr(shared_ptr const& copy) : data{copy.data}, count{copy.count}{
                 ++(*count);
             }
-            shared_ptr operator = (shared_ptr const& rhs){
-                //Temporarily preserve the older data members
-                T* olddata = data;
-                int* oldcount = count;
 
-                //Doing an exception safe transfer
-                data = rhs.data;
-                count = rhs.count;
-
-                //Updating the necessary counts
-                ++(*count);
-                --(*oldcount);
-
-                if (*oldcount == 0)
-                    delete data;
+            shared_ptr& operator = (shared_ptr rhs){
+                //Using copy and swap idom
+                rhs.swap(*this);
+                return *this;
             }
+
+            shared_ptr& operator = (T* ndata){
+                shared_ptr tmp(ndata);
+                tmp.swap(*this);
+                return *this;
+            }
+
+            void swap(shared_ptr& other) noexcept{
+                    std::swap(data, other.data);
+                    std::swap(count, other.count);
+            }
+
             //const correct owned object
             T* operator -> () const {return data;}
             T& operator * () const {return *data;}
@@ -38,4 +53,13 @@ namespace my_std{&
             T* get() const {return data;} //Getting access to smart pointer data
             explicit operator bool() const {return data;}
     };
+}
+
+int main(){
+    my_std::shared_ptr<int> sp(new int(4));
+    my_std::shared_ptr<int> sp2(sp);
+    my_std::shared_ptr<int> sp3(new int(6));
+    sp3 = sp2;
+    std::cout<<"\n"<<*sp3;
+    return 0;
 }
