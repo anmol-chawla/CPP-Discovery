b1b3f2dd9f894f886468434ab3d7c79fae826ead
Anmol Chawla <anmolchawla10@gmail.com>
2019-06-05 18:28:39 +0530

    Fix comment

--- Smart Pointers/my_unique_ptr.cpp
+++ Smart Pointers/my_unique_ptr.cpp
@@ -10,7 +10,7 @@ namespace my_std {
                 delete data;
             }
             unique_ptr(unique_ptr const&) = delete; //deleting the copy constructor
-            unique_ptr& operator = (unique_ptr const&) = delete; //deleting the equality operation
+            unique_ptr& operator = (unique_ptr const&) = delete; //deleting the assignment operator
             T* operator -> () const { return data; } //defining const explicitly for non modifying function
             T& operator * () const { return *data; } //defining const explicitly for non modifying function
             T* get() const { return data; } // getting access to smart pointer state
